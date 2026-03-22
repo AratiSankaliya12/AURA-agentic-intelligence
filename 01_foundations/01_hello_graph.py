86fdd09549976de7724c2a9f15201171b7bc1ca8
AratiSankaliya12 <aratisankaliya12@gmail.com>
2026-03-22 23:50:19 +0530

    written the hello_graph.py code

--- 01_foundations/01_hello_graph.py
+++ 01_foundations/01_hello_graph.py
@@ -1,1 +1,20 @@
-from typing 
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+
+class AgentState(TypedDict):
+    message: str
+
+
+def greeting_node(state: AgentState):
+    print("Processing greeting node...")
+    return {"message": state["message"] + " - Processed by Dhamu's Agent"}
+
+
+workflow = StateGraph(AgentState)
+
+workflow.add_node("greeting", greeting_node)
+workflow.add_edge(START, "greeting")
+workflow.add_edge("greeting", END)
+
+app = workflow.compile()
